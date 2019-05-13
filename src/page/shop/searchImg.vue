202e60dd62973ad07b8aa9de3daa387986d17027
chaosiling <906930360@qq.com>
2019-05-13 22:07:53 +0800

    1

--- src/page/shop/searchImg.vue
+++ src/page/shop/searchImg.vue
@@ -287,7 +287,11 @@
   import {
     getLocalStorage,
     setLocalStorage
-  } from '@/utils/mixin'
+  } from '@/utils/mixin';
+  import {
+    b64hamcsha1,
+    HexMD5
+  } from '@/utils/hash'
   import LoadMore from 'common/loadMore';
   import SearchBar from 'page/shop/searchBar';
   import axios from 'axios';
