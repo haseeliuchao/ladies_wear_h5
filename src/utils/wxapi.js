365ebfdfba4281d7da9dbc0796109ef5caaf0630
chaosiling <906930360@qq.com>
2019-05-15 23:03:18 +0800

    1

--- src/utils/wxapi.js
+++ src/utils/wxapi.js
@@ -21,9 +21,9 @@ const wxApi = {
       let data = Data.data// PS: 这里根据你接口的返回值来使用
       wx.config({
         debug: false, // 开启调试模式
-        appId: data.appId, // 必填，公众号的唯一标识
+        appId: data.app_id, // 必填，公众号的唯一标识
         timestamp: data.timestamp, // 必填，生成签名的时间戳
-        nonceStr: data.noncestr, // 必填，生成签名的随机串
+        nonceStr: data.nonce_str, // 必填，生成签名的随机串
         signature: data.signature, // 必填，签名，见附录1
         jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
       })
