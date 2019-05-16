004dff0263441bfde8ca826ccee150612841e867
chaosiling <906930360@qq.com>
2019-05-16 13:58:09 +0800

    1

--- src/page/shop/product.vue
+++ src/page/shop/product.vue
@@ -1177,7 +1177,7 @@ wxShareAppMessage () {
     let option = {
     title: '惠眼识货的这件商品还不错哦！赶紧过来下单吧', // 分享标题, 请自行替换
     desc: this.productInfo.title, // 分享描述, 请自行替换
-    link: window.location.href, // 分享链接，根据自身项目决定是否需要split
+    link: window.location.href.split('#')[0]+'#'+window.location.href.split('#')[1]+'?&', // 分享链接，根据自身项目决定是否需要split
     imgUrl: this.productInfo.imgList[1], // 分享图标, 请自行替换，需要绝对路径
     success: () => {
       // alert('分享成功')
