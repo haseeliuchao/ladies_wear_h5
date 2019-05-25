6233bc919252c6cf4dda6c70d8d9b70b483fac28
chaosiling <906930360@qq.com>
2019-05-25 16:24:55 +0800

    1

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -21,7 +21,7 @@ import {
 } from 'vuex';
 class Http {
   constructor(){
-    // this.Domain = 'http://tencent-ai.com/mop/';
+    this.Domain = 'http://tencent-ai.com/mop/';
     // this.Domain = 'http://192.168.9.187:8182';
     this.Domain = 'http://param.iask.in/mop/';
     
