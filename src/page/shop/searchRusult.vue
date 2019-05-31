4a1264777bbad83b2b3aa9ca41ff4fcfba05e8e6
kuangyx <kuangyx_218@163.com>
2019-05-31 11:28:00 +0800

    优化

--- src/page/shop/searchRusult.vue
+++ src/page/shop/searchRusult.vue
@@ -372,7 +372,7 @@
       <ul class="search-filter-list">
         <li :class="['search-filter-item',active==0 ? 'active' : '']" @click= "sortType(3)">综合排序</li>
         <li :class="['search-filter-item',active==1 ? 'active' : '']" @click= "sortType(1)">上新时间</li>
-        <li :class="['search-filter-item',active==2 ? 'active' : '']" @click= "sortType(2)">价格排序<span class="more-sort" :class="[sort_enumboo? 'more-sortAsc' : 'more-sortDesc']"></span></li>
+        <li :class="['search-filter-item',active==2 ? 'active' : '']" @click= "sortType(2)">价格排序<span class="more-sort" :class="[sort_enum==null?'':sort_enumboo? 'more-sortAsc' : 'more-sortDesc']"></span></li>
       </ul>
     </div>
     <!-- 筛选 -->
@@ -425,7 +425,7 @@
         },
         active:0,
         sort_enum:null,
-        sort_enumboo:false,
+        sort_enumboo:true,
         img_url:'',
       };
     },
@@ -451,7 +451,7 @@
       sortType(index){
         this.searchParams.sort_type=index;
          if(index==1){
-           this.active=1
+           this.active=1;
          }else if(index==2){
            this.active=2;
            this.sort_enum='ASC'
