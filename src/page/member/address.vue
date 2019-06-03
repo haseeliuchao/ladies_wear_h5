10cdfe77a75c85c302d370df842cdcdf5a4956e8
kuangyx <kuangyx_218@163.com>
2019-06-03 14:53:18 +0800

    优化

--- src/page/member/address.vue
+++ src/page/member/address.vue
@@ -179,7 +179,6 @@
        :class="['cell-btn',!errors.has('name')||errors.has('mobile')||errors.has('city')||errors.has('address')||addressForm.name.length==0||addressForm.phone.length==0||addressForm.address.length==0||addressForm.province.length==0||addressForm.city.length==0||addressForm.area.length==0?'disabled-btn':'']" 
       @click= "saveAddress">保存</div>
     </div>
-     :class="['cell-btn',(!errors.has('name')&&!errors.has('phone')&&!errors.has('city')?&&!errors.has('address'))?'':'disabled-btn']"
     <div>
       <mt-popup v-model="addressVisible" position="bottom"  class="address-pricker">
         <!-- <div class="address-picker-header">
