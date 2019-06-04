8698634ba2da4706fcc0302d1d5137ec8b11c1a5
chaosiling <906930360@qq.com>
2019-06-04 11:19:18 +0800

    1

---
+++ src/page/member/loginBlank.vue
@@ -0,0 +1,44 @@
+<style lang="scss" scoped>
+  @import '~assets/common/css/mixin.scss'; // @include flexbox()
+  
+</style>
+<template>
+  <div class="">
+   
+  </div>
+</template>
+<script>
+
+
+
+
+
+import {
+getLocalStorage,
+    setLocalStorage,
+    setSessionStorage,
+    getSessionStorage,pushHistory
+  } from '@/utils/mixin';
+  import {
+    Field,
+    Button,
+    Toast,
+    Popup
+  } from 'mint-ui';
+  export default {
+    data() {
+      return {
+      }
+    },
+    components: {},
+    methods: {
+      
+     
+    },
+    
+    mounted: function () {
+        
+    }
+  }
+
+</script>
