fd0976df4761128eef759d803e731a91cd454062
chaosiling <906930360@qq.com>
2019-06-18 10:13:37 +0800

    1

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"http://192.168.11.242:8080"'
+  // API_ROOT: '"http://192.168.11.242:8080"'
+  API_ROOT: '"http://param.iask.in/mop"'
 })
