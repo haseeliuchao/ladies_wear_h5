f5f4b074da89d14da76c3b8dff405cbeadbfd778
chaosiling <906930360@qq.com>
2019-06-24 14:06:35 +0800

    1

--- src/page/shopb/goodedit.vue
+++ src/page/shopb/goodedit.vue
@@ -55,8 +55,8 @@
         line-height: normal;
         color: #666;
       }
-      .ishow i:last-child{
-        display: none
+      em .ishow:last-child{
+          display: none
       }
     }
     .profit{
@@ -197,7 +197,11 @@
    <div class="goodskuedit" v-for="(item,index) in goodeditdata.item_sku" :key="index">
      <p class="color"><span>颜色</span><span style="color:#666">{{item.color}}</span></p>
      <p class="size"><span>尺码</span>
-     <span style="color:#666" v-for="(itemsize,index1) in sizearr" :key="index1"><em class="ishow" v-if="itemsize.color==item.color">{{itemsize.size}}<i>,</i></em></span></p>
+     <span style="color:#666" v-for="(itemsize,index1) in sizearr"  :key="index1">
+         <em v-if="itemsize.color==item.color">
+         <i>{{itemsize.size}}</i><i class="ishow">,</i>
+         </em>
+       </span></p>
      <p class="setprice"><span>设置售价</span><input style="text-align:right;" type="text" @keyup.prevent="changeSales(item,item.sales_consumer_price)" v-model="item.sales_consumer_price"></p>
      <p class="profit"><span>利润</span><span style="color:#666">{{item.profit/100|TwoNum}}</span></p>
    </div>
