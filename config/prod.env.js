44f44e4678bdefbd1ae92fabd8c1506f2de14198
chaosiling <906930360@qq.com>
2019-06-24 11:38:24 +0800

    1

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // API_ROOT: '"http://param.iask.in/mop"'
-  API_ROOT: '"http://192.168.9.75:8182"'
+  API_ROOT: '"http://param.iask.in/mop"'
 }
