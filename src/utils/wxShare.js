31a0458d77513fc2692ecafaf36f3ad2ef0f974d
chaosiling <906930360@qq.com>
2019-06-28 17:25:33 +0800

    1

--- src/utils/wxShare.js
+++ src/utils/wxShare.js
@@ -23,7 +23,7 @@ export default function wxShare({ title, desc, link, imgUrl} = {}) {
             timestamp: wxData.timestamp, // 必填，生成签名的时间戳
             nonceStr: wxData.nonce_str, // 必填，生成签名的随机串
             signature: wxData.signature, // 必填，签名，见附录1
-            jsApiList: ['showToolbar','hideOptionMenu','showOptionMenu','onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            jsApiList: ['hideToolbar','hideOptionMenu','showOptionMenu','onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
         })
     }
     if(isWeiXin('code')){
@@ -37,7 +37,7 @@ export default function wxShare({ title, desc, link, imgUrl} = {}) {
             wx.showOptionMenu()
         }
         window.addEventListener('scroll',function(){
-            wx.showToolbar()
+            wx.hideToolbar()
             },true)
         wx.onMenuShareAppMessage({
             title: title, // 分享标题
