1b91fafe538f5c02fb60604cdd0036d7385db9d9
chaosiling <906930360@qq.com>
2019-06-28 11:19:31 +0800

    1

--- src/page/shop/searchRusult.vue
+++ src/page/shop/searchRusult.vue
@@ -545,7 +545,6 @@
         this.searchParams.page_size = 10;
         this.searchParams.current_page = 1;
         this.searchRusultData=[];
-        this.searchParams.img_url=null;
         this.searchParams = JSON.parse(JSON.stringify(Object.assign(this.searchParams,this.$route.query)))
         this.searchParams.img_url=null;
         this.$refs.searchRusultloadMore.onTopLoaded(this.$refs.searchRusultloadMore.uuid);
@@ -607,8 +606,7 @@
           this.searchRusultData=[];
           this.active=1;
           this.sort_enum=null;
-          this.searchParams.img_url=null;
-          this.searchParams.searchParams.sort_enum=null;
+          this.searchParams.sort_enum=null;
           this.searchParams.sort_type = 1;
           this.searchParams = JSON.parse(JSON.stringify(Object.assign(this.searchParams,this.$route.query)
           ));
