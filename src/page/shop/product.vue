04432953a024ab202f947d6d6f30c7dcb2f1ab95
chaosiling <906930360@qq.com>
2019-07-12 10:43:50 +0800

    1

--- src/page/shop/product.vue
+++ src/page/shop/product.vue
@@ -1785,10 +1785,10 @@ methods: {
             return;
           }
           if(value.indexOf("img1.vvic.com")!=-1){
-            return value.replace('https://img1.vvic.com','http://param.iask.in/vvic/img1');
+            return value.replace('https://img1.vvic.com','http://m.chaochujue.cn/vvic/img1');
           }
           if(value.indexOf("img.vvic.com")!=-1){
-            return value.replace('https://img.vvic.com','http://param.iask.in/vvic/img');
+            return value.replace('https://img.vvic.com','http://m.chaochujue.cn/vvic/img');
           }
           if(value.indexOf("alicdn.com")!=-1){
             return value;
