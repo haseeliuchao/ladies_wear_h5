80ac8540a2e89452fc78434016e4060abafc7c7a
chaosiling <906930360@qq.com>
2019-08-06 16:19:26 +0800

    1

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // API_ROOT: '"http://param.iask.in/mop"'
-  API_ROOT: '"https://mop.chaochujue.cn"'
+  API_ROOT: '"http://param.iask.in/mop"'
+  // API_ROOT: '"https://mop.chaochujue.cn"'
 }
